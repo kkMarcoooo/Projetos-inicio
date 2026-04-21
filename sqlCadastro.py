49bb35d352bf4f6ee106fc62e442babeb1f59622
kkMarcoooo <marcosouzavitto11@gmail.com>
2026-04-21 12:43:15 -0300

    Refactor user management for betting system

--- sqlCadastro.py
+++ sqlCadastro.py
@@ -1,95 +1,140 @@
-import sqlite3
-conexao = sqlite3.connect("Banco_dados_legal.db")
-funcio = conexao.cursor()
-funcio.execute("""
-    CREATE TABLE IF NOT EXISTS login(
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        email TEXT NOT NULL,
-        senha TEXT NOT NULL
-)    
+import random, sqlite3
+import time
+
+conexao = sqlite3.connect("BD_aposta1.db")
+cursor = conexao.cursor()
+
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS dados_users(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome TEXT NOT NULL,
+    email TEXT NOT NULL,
+    senha TEXT NOT NULL,
+    saldo REAL NOT NULL
+)
 """)
-def cadastro():
-    while True:
-        print("--== CADASTRO ==--")
-        e = email()
-        s = senha()
-        funcio.execute("INSERT INTO login(email,senha) VALUES (?,?)", (e,s))
-        conexao.commit()
-        print("Usuário Cadastrado!")
-        continuar = str(input("Continuar? [S/N]: ").upper())
-        if continuar == "S":
-            continue
-        else:
-            break
-def login():
-    while True:
-        print("--== LOGIN ==--")
-        confirmar_email = confi_email()
-        if confirmar_email:
-            email_encontrado = confirmar_email[0]
-            login_certo = confi_senha(email_encontrado)
-            if login_certo:
-                print("Bem VIndo ao Sistema!")
-        else:
-            print("Email não encontrado no sistema.")
+
+
 def email():
     while True:
-        email = str(input("Email: "))
-        if email.endswith(("@gmail.com", "@yahoo.com", "@outlook.com", "@hotmail.com")):
-            return email
-        print("Insira email válido!")
+        new_email = str(input("Email: "))
+        if new_email.endswith(("@gmail.com", "@outlook.com", "@yahoo.com", "@hotmail.com")):
+            cursor.execute("SELECT * FROM dados_users WHERE email = ?", (new_email,))
+            resultado = cursor.fetchone()
+            if resultado:
+                print("E-mail já cadastrado!")
+            else:
+                return new_email
+        else:
+            print("Email inválido!")
+
+
+def nickname():
+    while True:
+        name = str(input("Apelido: "))
+        cursor.execute("SELECT * FROM dados_users WHERE nome = ?", (name,))
+        resultado_name = cursor.fetchone()
+        if resultado_name:
+            print("Apelido já registrado!")
+        else:
+           return name
+
+
 def senha():
     while True:
-        senha = str(input("Senha: "))
-        senhanums = len(senha)
-        if senhanums >= 6 and senhanums <= 20:
-            senhaconf = str(input("Confirme a senha: "))
-            if senhaconf == senha:
-                return senha
-            print("Senhas não coincidem!")
+        new_password = str(input("Senha: "))
+        caracteres = len(new_password)
+        if caracteres > 5 and caracteres < 21:
+            conf_password = str(input("Confirme a senha: "))
+            if conf_password == new_password:
+                return new_password
+            else:
+                print("SENHAS NÃO COINCIDEM!")
         else:
-            print("Digite uma senha de 6 á 20 carateres!")
-def confi_email():
-    emailLogin = str(input("Email: "))
-    funcio.execute("SELECT email FROM login WHERE email = ?",(emailLogin,))
-    return funcio.fetchone()
-def confi_senha(emailLogin):
-    tentativa = 3
-    funcio.execute("SELECT senha FROM login WHERE email = ?", (emailLogin,))
-    usuario = funcio.fetchone()
-    if usuario is None:
-        print("Usuário não encontrado.")
-        return False
-    senha = usuario[0]
-    while tentativa > 0:
-        senhalogin = str(input("Senha: "))
-        if senhalogin == senha:
-            print("LOGIN CONCEDIDO!")
-            return True
-        if tentativa > 0:
-            tentativa-=1
-            print(f"SENHA INCORRETA! Você tem {tentativa} tentativas restantes!")
-    print("Aceso bloqueado!")
-    conexao.close()
-    exit()
+            print("INSIRA UMA SENHA DE 6 A 20 CARACTERES!")
+
+
+def login_email():
+    while True:
+        email_login = str(input("E-mail: "))
+        cursor.execute("SELECT * FROM dados_users WHERE email = ?", (email_login,))
+        resultado_email_login = cursor.fetchone()
+        if resultado_email_login:
+            return email_login
+        else:
+            print("E-mail não encontrado no sistema!")
+            continue
+
+
+def login_senha(email1):
+    tentativas = 3
+    while tentativas > 0:
+        senha_login = str(input("Senha: "))
+        cursor.execute("SELECT nome FROM dados_users WHERE email = ? AND senha = ?", (email1, senha_login))
+        resultado_senha = cursor.fetchone()
+        if resultado_senha:
+            nome_login = resultado_senha[0]
+            print(f"LOGIN FEITO!\nSeja bem vindo {nome_login}!")
+            return email1
+        else:
+            tentativas -= 1
+            print(f"Erro! Você ainda tem {tentativas} tentativas!")
+    if tentativas == 0:
+        print("ACESSO BLOQUEADO")
+        print("...")
+        time.sleep(1.5)
+        cursor.close()
+
+
 while True:
-    print("\n--- SISTEMA DE ACESSO ---")
-    print("1. Cadastrar Novo Usuário")
-    print("2. Fazer Login")
-    print("3. Sair")
-    opcao = input("Escolha uma opção: ")
-    if opcao == "1":
-        cadastro()
-    elif opcao == "2":
-        login()
-    elif opcao == "3":
-        print("Saindo...")
-        conexao.close()
-        break
-    elif opcao == "000":
-        funcio.execute("SELECT * FROM login")
-        dados = funcio.fetchall()
-        for linha in dados:
-            print(linha)
+    action = int(input("| [1] Cadastrar | [2] Login |\nR: "))
+    if action in (1,2):
+        if action == 1:
+            email = email()
+            nome = nickname()
+            senhaa = senha()
+            saldo = 100
+            cursor.execute("INSERT INTO dados_users (nome, email, senha, saldo) VALUES (?, ?, ?, ?)",(nome, email, senhaa, saldo))
+            conexao.commit()
+            print(f"Cadastro Feito, {nome}! Voltando para menu inicial...\n")
+            time.sleep(2)
+            continue
+        if action == 2:
+            email1 = login_email()
+            email_login = login_senha(email1)
+            break
+print("▶►"*10, "◄◀"*10)
+print(" "*8,"👾🎲Pequeno Tigre🎲👾")
+print("▶►"*10, "◄◀"*10)
+while True:
+    cursor.execute("SELECT saldo FROM dados_users WHERE email = ?", (email_login,))
+    saldo = cursor.fetchone()[0]
+    print("—" * 20)
+    print(f"Saldo: R$ {saldo}")
+    print("—" * 20)
+    aposta = float(input("\nValor da aposta: "))
+    if aposta <= saldo:
+        roleta = random.randint(1, 8)
+        saldo -= aposta
+        sorteado = int(input("\nDigite o número secreto de 1 á 8: "))
+        print("\n")
+        if sorteado == roleta:
+            print("‖ WIN! ‖")
+            saldo += aposta * 2
+        else:
+            print("‖ You lost! ‖")
+            print(f"\nO número secreto era {roleta}!\n")
+        print("—" * 20)
+        print(f"Saldo: R$ {saldo}")
+        print("—" * 20)
+        cursor.execute("UPDATE dados_users SET saldo = ? WHERE email = ?", (saldo, email_login))
+        conexao.commit()
+        cont = str(input("Continuar [S/N]: ").upper())
+        if cont == "N":
+            print("Saindo do sistema..")
+            time.sleep(1)
+            break
+        else:
+            continue
     else:
-        print("Opção inválida!")
+        print("SALDO INSUFICIENTE!")
