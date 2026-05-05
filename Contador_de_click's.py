190f03365aa8fda62558d039a361d6f62f8742e6
kkMarcoooo <marcosouzavitto11@gmail.com>
2026-05-05 16:49:00 -0300

    Enhance click counter
    Added a feature to display a message when the click count reaches 67 and hide it when it exceeds 67.

--- Contador_de_click's.py
+++ Contador_de_click's.py
@@ -1,10 +1,18 @@
 import tkinter as tk
+
 janela = tk.Tk()
 contador = 0
+texto_67 = None
 def click():
-    global contador
+    global contador, texto_67
     contador += 1
     texto.config(text=f"{contador} cliques")
+    if contador == 67:
+        texto_67 = tk.Label(janela, text="SEIS SETE SEIS SETE SEIS SETE")
+        texto_67.place(relx=0.5,rely=0.8, anchor="center")
+    if contador >67:
+        if texto_67:
+            texto_67.place_forget()
 janela.config(background="#daff75")
 janela.geometry('900x700')
 janela.maxsize(width=1800, height=1800)
@@ -15,7 +23,8 @@ botao = tk.Button(
     text = "Botão",
     command=click,
     bg="#bd1e1e",
-    activebackground= "#e00000"
+    activebackground= "#e00000",
+    font=("Arial", 20)
 )
 botao.place(relx=0.5, rely= 0.5, anchor="center")
 
@@ -24,7 +33,7 @@ titulo.config(font=("Times New Roman", 20, "bold"))
 titulo.place(relx=0.5, rely=0.15, anchor="center")
 
 texto = tk.Label(janela, text = f"{0} cliques", bg="#fffbf0")
-texto.config(font=("bold"))
+texto.config(font=("Arial", 20))
 texto.place(relx=0.5, rely= 0.7, anchor="center")
 
 
