5c67e4ad21429d4ace94d3c5ebbc3fa6674c625c
kkMarcoooo <marcosouzavitto11@gmail.com>
2026-05-05 15:00:08 -0300

    Add files via upload

---
+++ Contador_de_click's.py
@@ -0,0 +1,31 @@
+import tkinter as tk
+janela = tk.Tk()
+contador = 0
+def click():
+    global contador
+    contador += 1
+    texto.config(text=f"{contador} cliques")
+janela.config(background="#daff75")
+janela.geometry('900x700')
+janela.maxsize(width=1800, height=1800)
+janela.minsize(width=400, height=300)
+
+botao = tk.Button(
+    janela,
+    text = "Botão",
+    command=click,
+    bg="#bd1e1e",
+    activebackground= "#e00000"
+)
+botao.place(relx=0.5, rely= 0.5, anchor="center")
+
+titulo = tk.Label(janela,text= "Contador de cliques", bg="#daff75")
+titulo.config(font=("Times New Roman", 20, "bold"))
+titulo.place(relx=0.5, rely=0.15, anchor="center")
+
+texto = tk.Label(janela, text = f"{0} cliques", bg="#fffbf0")
+texto.config(font=("bold"))
+texto.place(relx=0.5, rely= 0.7, anchor="center")
+
+
+janela.mainloop()
